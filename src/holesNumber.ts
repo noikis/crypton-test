80fd033f8424f66491c6ee1633c8c57c73648345
Ezzahid Ilyas <ezzil1997@gmail.com>
2021-12-23 14:40:36 +0700

    build: task 2 => numbers of hole

---
+++ src/holesNumber.ts
@@ -0,0 +1,32 @@
+interface NumWithHoles {
+  number: number;
+  holes: number;
+}
+
+export default function holesNumber(nums: number[]): NumWithHoles[] {
+  // Split numbers
+  const numbersAsStr = nums.map((num) => num.toString().split(""));
+  let result = [] as NumWithHoles[];
+
+  for (const numsAsStr of numbersAsStr) {
+    let holes = 0;
+
+    // Check  holes
+    for (let numAsStr of numsAsStr) {
+      if (["4", "6", "9", "0"].includes(numAsStr)) {
+        holes = holes + 1;
+      } else if (numAsStr === "8") {
+        holes = holes + 2;
+      }
+    }
+
+    const numWithHoles: NumWithHoles = {
+      number: Number(numsAsStr.join("")),
+      holes,
+    };
+
+    result.push(numWithHoles);
+  }
+
+  return result.sort((a, b) => a.holes - b.holes);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { testMarks, getStatistics } from "./getStatistics";
+import holeNumbers from "./holesNumber";
 
 // Test 1
 console.log("\n Test 1 => get Statistics: ");
 console.log(getStatistics(testMarks));
+
+// Test 2
+console.log("\n Test 2 => numbers of holes: ");
+console.log(holeNumbers([1, 6, 480, 80, 62, 3]));
