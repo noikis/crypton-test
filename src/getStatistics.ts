6e13fcdcb528cfaee7450daea1e23d2f745923b8
Ezzahid Ilyas <ezzil1997@gmail.com>
2021-12-23 14:07:43 +0700

    build: task 1 => getStatistics

---
+++ src/getStatistics.ts
@@ -0,0 +1,42 @@
+interface Student {
+  name: string;
+  avgMark: number;
+}
+
+interface Statistics {
+  avgMark: number;
+  highestMark: string;
+  lowestMark: string;
+}
+
+export const testMarks: Student[] = [
+  {
+    name: "Vasya",
+    avgMark: 3.75,
+  },
+  {
+    name: "Lena",
+    avgMark: 4.89,
+  },
+];
+
+export function getStatistics(marks: Student[]): Statistics {
+  const scores: number[] = marks.map((mark) => mark.avgMark);
+
+  const avgMark =
+    scores.reduce((acc, current) => acc + current, 0) / scores.length;
+
+  const highestMark = marks.find(
+    (mark) => mark.avgMark === Math.max(...scores)
+  ).name;
+
+  const lowestMark = marks.find(
+    (mark) => mark.avgMark === Math.min(...scores)
+  ).name;
+
+  return {
+    avgMark,
+    highestMark,
+    lowestMark,
+  };
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { testMarks, getStatistics } from "./getStatistics";
+
+// Test 1
+console.log("\n Test 1 => get Statistics: ");
+console.log(getStatistics(testMarks));
